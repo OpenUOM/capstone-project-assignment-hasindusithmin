c19b4d9b0c2e1fc5e4f6639b0b0b45b31e5e1a34
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-07 11:32:30 +0530

    Update the updateStudent function to update details of a specific student.

--- backend/database.js
+++ backend/database.js
@@ -120,11 +120,11 @@ const readStudentInfo = async (id) => {
     });
 }
 
-const addStudent = async (id, name, age, hometown) => {
+const addStudent = async (id, name, age, religion) => {
     const sql = `INSERT INTO student(id,name,age,hometown) VALUES (?,?,?,?)`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [id, name, age, hometown])
+            .raw(sql, [id, name, age, religion])
             .then(() => {
                 resolve({status: "Successfully inserted Student"})
             })
@@ -135,7 +135,7 @@ const addStudent = async (id, name, age, hometown) => {
 }
 
 const updateStudent = async (name, age, religion, id) => {
-    const sql = `SELECT * FROM dummyData`
+    const sql = `UPDATE student SET name=?, age=?, hometown=? WHERE id=?`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
